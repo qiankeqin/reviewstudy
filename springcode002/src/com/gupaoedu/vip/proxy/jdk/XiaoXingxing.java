d43f38750253fd8c82449e72b98a5816835b2301
qiankeqin <951669478@qq.com>
2018-05-05 19:03:08 +0800

    class create

---
+++ springcode002/src/com/gupaoedu/vip/proxy/jdk/XiaoXingxing.java
@@ -0,0 +1,10 @@
+package com.gupaoedu.vip.proxy.jdk;
+
+public class XiaoXingxing implements Person {
+    @Override
+    public void findLove() {
+        System.out.println("长得帅！");
+        System.out.println("有房有车");
+        System.out.println("高1.8以上");
+    }
+}
